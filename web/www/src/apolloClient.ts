7ff095202e2a4da9f4328383ca7f394399be601f
Luboš Matejčík <lubos.matejcik@gmail.com>
2022-07-18 08:47:35 +0200

    fix: build

--- web/www/src/apolloClient.ts
+++ web/www/src/apolloClient.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloClient, InMemoryCache } from "@apollo/client/core/index"
 
 const apolloClient = new ApolloClient({
   uri: import.meta.env.VITE_API_URL,
